fab4cf2f62224e82b4bec5cfe5fc3b10057c5d84
inayuky <inayuky@gmail.com>
2021-11-03 07:48:37 +0900

    add password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 100}
   validates :email, presence: true
   validates :password, presence: true
+
+  has_secure_password
 end
