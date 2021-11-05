58443b71ff7e3983496665ffa64a92a5e0211559
inayuky <inayuky@gmail.com>
2021-11-05 16:42:49 +0900

    destroy post when destroy user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 100}
   validates :email, presence: true
   validates :password, presence: true
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   has_secure_password
 end
